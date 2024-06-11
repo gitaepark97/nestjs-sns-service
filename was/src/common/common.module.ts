0f711cac69ce363ced741c37bcbff9bd4bdfe575
gitaepark97 <parkkitae7@gmail.com>
2024-06-11 11:15:13 +0900

    feat: Health Check

--- was/src/common/common.module.ts
+++ was/src/common/common.module.ts
@@ -1,23 +1,8 @@
-import {
-  Global,
-  Logger,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-} from "@nestjs/common";
-import { Environment } from "../config/env.validation";
-import { LoggingMiddleware } from "./logging/logging.middleware";
-import { winstonLogger } from "./logging/winston.logger";
+import { Module } from "@nestjs/common";
+import { LoggingModule } from "./logging/logging.module";
+import { HealthModule } from "./health/health.module";
 
-@Global()
 @Module({
-  providers: [{ provide: Logger, useValue: winstonLogger }],
-  exports: [Logger],
+  imports: [HealthModule, LoggingModule],
 })
-export class CommonModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    if (process.env.NODE_ENV !== Environment.Test) {
-      consumer.apply(LoggingMiddleware).forRoutes("*");
-    }
-  }
-}
+export class CommonModule {}
---
+++ was/src/common/logging/logging.module.ts
@@ -0,0 +1,23 @@
+import {
+  Global,
+  Logger,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from "@nestjs/common";
+import { winstonLogger } from "./winston.logger";
+import { Environment } from "../../config/env.validation";
+import { LoggingMiddleware } from "./logging.middleware";
+
+@Global()
+@Module({
+  providers: [{ provide: Logger, useValue: winstonLogger }],
+  exports: [Logger],
+})
+export class LoggingModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    if (process.env.NODE_ENV !== Environment.Test) {
+      consumer.apply(LoggingMiddleware).forRoutes("*");
+    }
+  }
+}
